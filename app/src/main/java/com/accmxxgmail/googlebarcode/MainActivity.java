32b0af6fad58193cdb9532b4ba5bf79b6a6ed95a
Michael Xu <accmxx@gmail.com>
2017-07-06 00:47:34 -0400

    Initial commit

---
+++ app/src/main/java/com/accmxxgmail/googlebarcode/MainActivity.java
@@ -0,0 +1,48 @@
+package com.accmxxgmail.googlebarcode;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView barcodeResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        barcodeResult = (TextView) findViewById(R.id.barcodeResultTextView);
+    }
+
+    public void scanBarcode(View view){
+        Intent intent = new Intent(this, ScanBarcodeActivity.class);
+        startActivityForResult(intent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==0) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra("barcode");
+                    barcodeResult.setText("Barcode value : " + barcode.displayValue);
+                }
+                else{
+                    barcodeResult.setText("No barcode found");
+                }
+            }
+        }
+        else{
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
